e3c42adc8a957145eedcd853353d03231647ac0e
Gilvan Júnior <98542272+gilvancmjr@users.noreply.github.com>
2023-05-26 08:00:01 -0300

    update

--- src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,14 +1,25 @@
 package br.com.alurafood.pedidos.amqp;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import br.com.alurafood.pedidos.dto.PagamentosDto;
+
 @Component
 public class PagamentoListener {
+//deve salvar no banco mas no momente somente println
+    @RabbitListener(queues = "pagamentos.detalhes-pedido")
+    public void recebeMensagem(PagamentosDto pagamentosDto) {
+        String mensagem = """
+            Dados do pagamento: %s
+            Número do pedido: %s
+            Valor R$: %s
+            Status: %s 
+            """.formatted(pagamentosDto.getId(),
+            pagamentosDto.getPedidoId(),
+            pagamentosDto.getValor(),
+            pagamentosDto.getStatus());
 
-    @RabbitListener(queues = "pagamento.concluido")
-    public void recebeMensagem(Message mensagem) {
-        System.out.println("Recebi a mensagem " + mensagem.toString());
+    System.out.println("Recebi a mensagem " + mensagem);
     }
 }
